3f9df12c6e894d2091d1e6bbab11395543a9d6c9
GongHyeon Lee <108728631+GongHyeon@users.noreply.github.com>
2022-09-27 20:11:50 +0900

    Add files via upload

---
+++ self_practioce.cpp
@@ -0,0 +1,12 @@
+﻿#include <iostream>
+//#include <string>
+using namespace std;
+
+void main()
+{
+	int No_Int = 3;
+	int* pt;
+	pt = &No_Int;
+	cout << "정수내용" << No_Int << "정수주소" << &No_Int << endl;
+	cout << "Pt내용" << *pt << "    pt주소" << pt << endl;
+}
